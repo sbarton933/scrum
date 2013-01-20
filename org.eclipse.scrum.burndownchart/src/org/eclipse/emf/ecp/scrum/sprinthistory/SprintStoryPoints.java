0b7cb53c63f49c05fa84642156a4e1bc2c067924
shakti <shaktisaxena82@gmail.com>
2013-01-20 20:04:22 +0100

    Update org.eclipse.scrum.burndownchart/src/org/eclipse/emf/ecp/scrum/sprinthistory/SprintStoryPoints.java
    unused function removed

--- org.eclipse.scrum.burndownchart/src/org/eclipse/emf/ecp/scrum/sprinthistory/SprintStoryPoints.java
+++ org.eclipse.scrum.burndownchart/src/org/eclipse/emf/ecp/scrum/sprinthistory/SprintStoryPoints.java
@@ -1,64 +1,41 @@
 package org.eclipse.emf.ecp.scrum.sprinthistory;
 
-import java.util.Comparator;
 import java.util.Date;
-import java.util.List;
 
-public class SprintStoryPoints  {
-	
-	 int perSprintStoryPoints;
-	
-	 Date dateEnteredForSprint;
-	
-	private SprintStoryPoints sprint;
-	
-	public SprintStoryPoints()
-	{
-		
+public class SprintStoryPoints {
+
+	int perSprintStoryPoints;
+
+	Date dateEnteredForSprint;
+
+	public SprintStoryPoints() {
+
 	}
-	
-	public SprintStoryPoints setSprintStoryPoints(int perSprintStoryPoints, Date dateEnteredForSprint) {
-		
+
+	public SprintStoryPoints setSprintStoryPoints(int perSprintStoryPoints,
+			Date dateEnteredForSprint) {
+
 		this.perSprintStoryPoints = perSprintStoryPoints;
 		this.dateEnteredForSprint = dateEnteredForSprint;
-		
-		return this;
-		 
-	}
-	
-	
-	public List<SprintStoryPoints> getSprintStoryPoints()
-	{
-		return null;
-		
-	}
 
+		return this;
+
+	}
 
 	public int getPerSprintStoryPoints() {
 		return perSprintStoryPoints;
 	}
 
-
 	public Date getDateEnteredForSprint() {
 		return dateEnteredForSprint;
 	}
-
 
 	public void setPerSprintStoryPoints(int perSprintStoryPoints) {
 		this.perSprintStoryPoints = perSprintStoryPoints;
 	}
 
-
 	public void setDateEnteredForSprint(Date dateEnteredForSprint) {
 		this.dateEnteredForSprint = dateEnteredForSprint;
 	}
-
-
-	
-
-
-	
-	
-	
 
 }
